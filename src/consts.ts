ac79b5bf6e3c64776249d3e9f766687e1d03b9b4
Cristian-Massa <cristianmassa30@gmail.com>
2024-04-27 19:00:25 -0300

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Cristian Massa';
+export const SITE_DESCRIPTION = 'Portafolio de trabajos de Cristian Massa como desarrollador web y blog personal';
